d2661e53d949f829b2a823c85ee36e526853a574
Rain <muther22@gmail.com>
2024-09-22 15:44:07 -0400

    added type hints in game functions

--- modules/game/GameFunctions.py
+++ modules/game/GameFunctions.py
@@ -15,7 +15,7 @@ class Game:
     def __init__(self):
         pass
 
-    def choose_suit(self,player,suit):
+    def choose_suit(self,player: str,suit: str) -> str:
         #check if the played suit is an option
         if suit.lower() not in self.suits:
             return "Invalid Suit Specified"
@@ -27,7 +27,7 @@ class Game:
 
         return f"{player} will play {suit}"
     
-    def end_turn(self):
+    def end_turn(self) -> str:
         turnDescription = f"Ending turn {self.turnCount}, with chosen suits:\n\n"
 
         for player,suit in self.playerChoices.items():
@@ -37,8 +37,7 @@ class Game:
         self.turnCount += 1
         return turnDescription.strip()
     
-    def reset(self):
+    def reset(self) -> None:
         self.turnCount = 1
         self.playerChoices = {}
-        return "Reset game state"
     
